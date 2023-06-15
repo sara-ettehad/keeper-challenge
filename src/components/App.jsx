512e2db9edbb3cd6da55fabfda9e07c980c2b9d0
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2023-06-14 20:07:15 -0400

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './footer';
+
+function App() {
+    return <div>
+        <Header/>
+        <Footer/>
+    </div>
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+    return <div>
+        <header>Parisa Ettehad</header>
+    </div>
+}
+
+export default Header;
